d8032e63a042de01b3ceb4045ea0e6ecdcddbff7
Arafat Husayn <hello@arafat.dev>
2020-02-04 23:52:05 +0600

    error passing and remove url after tab close

--- background.js
+++ background.js
@@ -1,16 +1,37 @@
-chrome.contextMenus.onClicked.addListener(function(item, tab) {
+let recordedTabId
 
-  chrome.storage.local.get(['image_url'], function(items) {
+chrome.contextMenus.onClicked.addListener((_item, tab) => {
+  chrome.storage.local.get(["image_url"], items => {
+    if (items && items.image_url) {
+      const url = items.image_url
 
-    chrome.tabs.create({url: items.image_url, index: tab.index + 1});
+      chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+        recordedTabId = tabs[0].id
 
-  });
+        chrome.tabs.create({ url: url, index: tab.index + 1 })
+      })
+    } else {
+      var errorMessage =
+        "No image found! Please click on the image and try again. \n\nIf the problem persists, please report issue here: \nhttps://github.com/arafathusayn/Google-Full-Image-Opener/issues"
 
-});
+      chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+        chrome.tabs.sendMessage(tabs[0].id, {
+          error: errorMessage,
+        })
+      })
+    }
+  })
+})
+
+chrome.tabs.onRemoved.addListener(removedTabId => {
+  if (recordedTabId === removedTabId) {
+    chrome.storage.local.remove(["image_url"])
+  }
+})
 
 chrome.contextMenus.create({
-  id: 'show_image',
-  title: 'Open Image Link Now!',
-  type: 'normal',
-  contexts: ['all'],
-});
+  id: "show_image",
+  title: "Open Image Link Now!",
+  type: "normal",
+  contexts: ["all"],
+})
